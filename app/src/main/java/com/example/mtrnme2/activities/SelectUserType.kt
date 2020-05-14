a3bbbc1d7a570f89e7b248c97e3ff9d49159c605
MTRNME <umais.625@gmail.com>
2020-05-14 14:52:37 +0500

    Instrument Fragment Issue

--- app/src/main/java/com/example/mtrnme2/activities/SelectUserType.kt
+++ app/src/main/java/com/example/mtrnme2/activities/SelectUserType.kt
@@ -1,13 +1,16 @@
 package com.example.mtrnme2.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.mtrnme2.R
 import com.example.mtrnme2.fragments.InstrumentFragment
 import com.example.mtrnme2.services.UserService
+import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.activity_select_user_type.*
+
 
 class SelectUserType : AppCompatActivity() {
     var uname: String? = null
@@ -19,12 +22,12 @@ class SelectUserType : AppCompatActivity() {
         setContentView(R.layout.activity_select_user_type)
 
         btn_done.setOnClickListener {
-            replaceFragment(InstrumentFragment())
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.icontainer, InstrumentFragment.newInstance())
+                    .commitNow()
         }
     }
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
-        fragmentTransaction.commit()
+
     }
 }
