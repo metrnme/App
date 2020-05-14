f83723fbbfb21f5950ad75fd3d5b302e7235ac42
Muhammad Zubair <Muhammadzubair906@gmail.com>
2020-05-14 05:45:09 +0500

    added viewmodel logic

--- app/src/main/java/com/example/mtrnme2/activities/viewmodels/TrackViewModel.kt
+++ app/src/main/java/com/example/mtrnme2/activities/viewmodels/TrackViewModel.kt
@@ -1,7 +1,19 @@
 package com.example.mtrnme2.activities.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.mtrnme2.models.Track
 
 class TrackViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun getTracks() : MutableList<Track>{
+        var listOfTracks = mutableListOf<Track>()
+        listOfTracks.add(Track("1", "Fly me to the moon", "https://google.com", "johndoe", "23"))
+        listOfTracks.add(Track("2", "Fly me to the moon 2", "https://google.com", "johndoe", "23"))
+        listOfTracks.add(Track("3", "Fly me to the moon 3", "https://google.com", "johndoe", "23"))
+        listOfTracks.add(Track("4", "Fly me to the moon 4", "https://google.com", "johndoe", "23"))
+        listOfTracks.add(Track("5", "Fly me to the moon 5", "https://google.com", "johndoe", "23"))
+        listOfTracks.add(Track("6", "Fly me to the moon 6", "https://google.com", "johndoe", "23"))
+
+        return listOfTracks
+    }
 }
--- app/src/main/java/com/example/mtrnme2/fragments/TrackFragment.kt
+++ app/src/main/java/com/example/mtrnme2/fragments/TrackFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mtrnme2.R
 import com.example.mtrnme2.activities.viewmodels.TrackViewModel
@@ -14,8 +15,6 @@ import com.example.mtrnme2.models.Track
 import kotlinx.android.synthetic.main.track_fragment.*
 
 class TrackFragment : Fragment() {
-
-    var listOfTracks : MutableList<Track> = mutableListOf()
     var tracksAdapter : TrackAdapter ?=null
 
     companion object {
@@ -33,16 +32,10 @@ class TrackFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TrackViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(TrackViewModel::class.java)
 
-        listOfTracks.add(Track("1", "Fly me to the moon", "https://google.com", "johndoe", "23"))
-        listOfTracks.add(Track("2", "Fly me to the moon 2", "https://google.com", "johndoe", "23"))
-        listOfTracks.add(Track("3", "Fly me to the moon 3", "https://google.com", "johndoe", "23"))
-        listOfTracks.add(Track("4", "Fly me to the moon 4", "https://google.com", "johndoe", "23"))
-        listOfTracks.add(Track("5", "Fly me to the moon 5", "https://google.com", "johndoe", "23"))
-        listOfTracks.add(Track("6", "Fly me to the moon 6", "https://google.com", "johndoe", "23"))
 
-        tracksAdapter = TrackAdapter(listOfTracks)
+        tracksAdapter = TrackAdapter(viewModel.getTracks())
 
         tracks.layoutManager = LinearLayoutManager(context)
         tracks.adapter = tracksAdapter
