07992b96193f5b3f3d09ddbf95a4c3a2e2cb8fcc
MTRNME <umais.625@gmail.com>
2020-07-27 21:36:31 +0500

    Upload Floating Action Button

--- app/src/main/java/com/example/mtrnme2/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/mtrnme2/activities/DashboardActivity.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.mtrnme2.R
 import com.example.mtrnme2.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
@@ -61,7 +62,12 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         btm_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        replaceFragment(HomeFragment())
+        replaceFragment(PlayerFragment())
+
+        FAB.setOnClickListener{
+            replaceFragment(UploadFragment())
+        }
+
     }
 
     private fun replaceFragment(fragment: Fragment){
