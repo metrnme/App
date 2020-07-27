3c98b3a26ac45cc8b4ca2ea17bd71e657cf6d491
MTRNME <umais.625@gmail.com>
2020-07-28 02:03:32 +0500

    Tracks clickable, and playable using web links

--- app/src/main/java/com/example/mtrnme2/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/mtrnme2/activities/DashboardActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.mtrnme2.R
 import com.example.mtrnme2.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,17 +19,20 @@ class DashboardActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {item->
         when(item.itemId){
             R.id.Home_ic->{
-                replaceFragment(PlayerFragment())
+                /*replaceFragment(PlayerFragment())*/
+                findNavController(R.id.myNavHostFragment).navigate(R.id.nav_home)
                 Log.d("app:Print1", "Home Is Pressed")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.Search_ic->{
-                replaceFragment(SearchFragment())
+                /*replaceFragment(SearchFragment())*/
+                findNavController(R.id.myNavHostFragment).navigate(R.id.nav_search)
                 Log.d("app:Print1", "Search Is Pressed")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.Library_ic->{
-                replaceFragment(TrackFragment())
+                /*replaceFragment(TrackFragment())*/
+                findNavController(R.id.myNavHostFragment).navigate(R.id.nav_track)
                 Log.d("app:Print1", "Track Fragment Is Pressed")
                 return@OnNavigationItemSelectedListener true
                //val intent = Intent(this@DashboardActivity, TrackActivity::class.java)
@@ -61,8 +66,10 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        val navigationController = findNavController(R.id.myNavHostFragment)
+
         btm_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        replaceFragment(PlayerFragment())
+        navigationController.navigate(R.id.nav_home)
 
         FAB.setOnClickListener{
             replaceFragment(UploadFragment())
