37fad2a210ee23073e2a5d293abfaf4d44dcdb14
Asad Saeed <asad.saeed@live.com>
2020-07-29 09:28:03 +0500

    Mediaplayer set

--- app/src/main/java/com/example/mtrnme2/fragments/UploadFragment.kt
+++ app/src/main/java/com/example/mtrnme2/fragments/UploadFragment.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -37,7 +39,6 @@ class UploadFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         binding.btnBrowseart.setOnClickListener { view ->
             Snackbar.make(view, "BROWSE!", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
